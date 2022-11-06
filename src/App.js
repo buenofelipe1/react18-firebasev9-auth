ff96d97bcd7fcfd55aa734092e110a4dff282414
buenofelipe1 <49288188+buenofelipe1@users.noreply.github.com>
2022-11-06 14:18:03 -0300

    firstcomit

--- src/App.js
+++ src/App.js
@@ -3,18 +3,27 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
 import Login from "./components/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
 import Signup from "./components/Signup";
+import { UserAuthContextProvider } from "./context/userAuthContext";
 
 function App() {
   return (
     <Container style={{ width: "400px" }}>
       <Row>
         <Col>
+        <UserAuthContextProvider>
           <Routes>
-            <Route path="/home" element={<Home />} />
+            <Route path="/home"
+                   element={
+                    <ProtectedRoute>
+                   <Home />
+                   </ProtectedRoute>
+                   } />
             <Route path="/" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
           </Routes>
+        </UserAuthContextProvider>
         </Col>
       </Row>
     </Container>
